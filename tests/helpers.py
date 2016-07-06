f7af483926cbf823c9207004cb3f89ad9e7916c1
Hannes Ovrén <hannes.ovren@liu.se>
2016-07-06 16:34:47 +0200

    Dataset builder and tests

---
+++ tests/helpers.py
@@ -0,0 +1,45 @@
+import numpy as np
+from datasetgen.maths.quaternions import Quaternion, QuaternionArray
+from numpy import testing as nt
+
+from crisp.fastintegrate import integrate_gyro_quaternion_uniform
+
+def random_position():
+    return np.random.uniform(-10, 10, size=3)
+
+
+def random_orientation():
+    n = np.random.uniform(-1, 1, size=3)
+    n /= np.linalg.norm(n)
+    phi = np.random.uniform(-np.pi, np.pi)
+    qparts = (np.cos(phi/2), n * np.sin(phi/2))
+    qdata = np.hstack(qparts)
+    q = Quaternion(*qdata)
+    return q
+
+
+def random_focal():
+    return np.random.uniform(100., 1000.)
+
+def unpack_quat(q):
+    return np.array([q.w, q.x, q.y, q.z])
+
+def gyro_data_to_quaternion_array(gyro_data, gyro_times):
+    dt = float(gyro_times[1] - gyro_times[0])
+    nt.assert_almost_equal(np.diff(gyro_times), dt)
+    q = integrate_gyro_quaternion_uniform(gyro_data, dt)
+    return QuaternionArray(q)
+
+
+def find_landmark(p, landmarks):
+    if not landmarks:
+        return None
+
+    lm = next((lm for lm in landmarks if np.all(lm.position == p)), None)
+    return lm
+
+#    best = min(landmarks, key=lambda lm: np.linalg.norm(lm.position - p))
+#    if np.allclose(best.position, p):
+#        return best
+#    else:
+#        return None
