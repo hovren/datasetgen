361f34ee5480416d2a0f96f517553d31c1ae9749
Hannes Ovrén <hannes.ovren@liu.se>
2017-03-02 18:52:48 +0100

    Update README and setup

--- setup.py
+++ setup.py
@@ -1,21 +1,7 @@
 #!/usr/bin/env python
 #
-# Copyright (C) 2009-2011 University of Edinburgh
+# Copyright (C) 2015-2017 Hannes Ovrén
 #
-# This file is part of IMUSim.
-#
-# IMUSim is free software: you can redistribute it and/or modify it
-# under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# IMUSim is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with IMUSim.  If not, see <http://www.gnu.org/licenses/>.
 
 from __future__ import print_function
 depsOK = True
